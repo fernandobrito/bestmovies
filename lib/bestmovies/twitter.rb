ad2ba6b40fa1dc081937a7aeb2c27c7bd1a83b82
Fernando Brito <email@fernandobrito.com>
2010-11-12 18:27:24 -0300

    Handle the cases when the movie title is too long to be tweeted
    This code needs to be refactored ASAP. It is really repetitive, but it works

--- lib/bestmovies/twitter.rb
+++ lib/bestmovies/twitter.rb
@@ -30,10 +30,18 @@ module BestMovies::Twitter
     puts "-- Ends: #{ends.strftime('%d/%m/%Y %H:%M')}\n"
 
     events.each_with_index do |event, index|
+      # The message to be tweeted. By default it has the movie title (pt-BR) and the original movie title
       output = "[ #{time} ] #{(event.begins-60*60).strftime('%H:%M')} .:. #{event.channel.name} .:. #{event.movie.title} (#{event.movie.original_title}) .:. Nota: #{event.movie.score} .:. Ano: #{event.movie.year} .:. Gênero: #{event.movie.gender.name} .:. [GMT -3]"
 
+      # If there is no original title, or its the same as the normal title or the message is too big, remove the original_title
       if event.movie.original_title.nil? or event.movie.original_title == event.movie.title or output.size > 140
         output = "[ #{time} ] #{(event.begins-60*60).strftime('%H:%M')} .:. #{event.channel.name} .:. #{event.movie.title} .:. Nota: #{event.movie.score} .:. Ano: #{event.movie.year} .:. Gênero: #{event.movie.gender.name} .:. [GMT -3]"
+      end
+
+      # If it still too long, we have to remove the last chars of the movie title
+      if output.size > 140
+        max_title_size = -(output.size - event.movie.title.size - 140)
+        output = "[ #{time} ] #{(event.begins-60*60).strftime('%H:%M')} .:. #{event.channel.name} .:. #{event.movie.title[0...47]}... .:. Nota: #{event.movie.score} .:. Ano: #{event.movie.year} .:. Gênero: #{event.movie.gender.name} .:. [GMT -3]"
       end
 
       puts "#{index+1}: #{output}"
