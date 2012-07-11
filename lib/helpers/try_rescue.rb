38ec05037978f4e4b619df7bdfb47eb91df43580
Fernando Brito <email@fernandobrito.com>
2012-07-10 23:13:24 -0300

    When rescuing, print the backtrace

--- lib/helpers/try_rescue.rb
+++ lib/helpers/try_rescue.rb
@@ -2,6 +2,7 @@ def try times = 1, options = {}, &block
   val = yield
 rescue (options[:on] || Exception) => e
   puts "************ Rescued something! #{e} (#{e.class})!"
+  puts e.backtrace
   retry if (times -= 1) > 0 and e.class != Interrupt
 else
   val
